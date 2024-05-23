cd9c224116a2ee70ce48bd280864b173051153f9
xcgh-rax-rax <lpatten1729@gmail.com>
2024-05-23 14:51:56 +0100

    complete NetbiosName class

--- netbios_name.py
+++ netbios_name.py
@@ -1,19 +1,44 @@
 from typing import List, Optional
+from dataclasses import dataclass
+import struct
 
+@dataclass
 class NetBIOSName:
-    def __init__(self, raw_netbios_name: bytes):
-        self.raw_netbios_name = raw_netbios_name
+    encoded_netbios_name: bytes
 
     @classmethod
-    def build_from_string(cls, netbios_name: str) -> 'NetBIOSName':
-        labels: List[str] = netbios_name.splet('.')
-        domain_length: int = 0
+    def build_from_string(cls, netbios_name: str, scope_id: str) -> "NetBIOSName":
+        first_level_encoded: str = cls.first_level_encode(netbios_name, scope_id)
+        second_level_encoded: bytes = cls.second_level_encode(first_level_encoded)
+        return cls(second_level_encoded)
+
+    @staticmethod
+    def first_level_encode(netbios_name: str, scope_id: str) -> str:
+        encoded_name: str = ""
+        for c in netbios_name:
+            c_ord: int = ord(c)
+            lower_nibble: int = c_ord & 0xF
+            upper_nibble: int = (c_ord & 0xF0) >> 4
+            encoded_name += chr(upper_nibble + ord("A"))
+            encoded_name += chr(lower_nibble + ord("A"))
+        return encoded_name + "." + scope_id
+
+    @staticmethod
+    def second_level_encode(domain_name: str) -> bytes:
+        encoded_domain_name: bytes = b""
+        labels: List[str] = domain_name.split(".")
         for label in labels:
-            name_bytes: bytes = label.encode('utf-8')
-            number_of_bytes: int = len(name_bytes)
-            if number_of_bytes > 63:
-                raise ValueError(f'Candiate NetBIOS name label "{netbios_name}" is longer than 63 bytes')
-            domain_length 
-        
-    def get_raw_netbios_name(self) -> bytes:
-        return self.raw_netbios_name
+            label_bytes: bytes = label.encode("utf-8")
+            label_length: int = len(label_bytes)
+            if label_length > 63:
+                raise ValueError(f'Candidate label "{label}" is longer than 63 bytes')
+            encoded_domain_name += struct.pack("B", label_length)
+            encoded_domain_name += label_bytes
+        encoded_domain_name += b"\x00"
+        print(len(encoded_domain_name))
+        if len(encoded_domain_name) > 255:
+            raise ValueError(
+                f'Encoded domain name "{domain_name}" exceeds maximum permissible length of 255 bytes'
+            )
+        return encoded_domain_name
+
